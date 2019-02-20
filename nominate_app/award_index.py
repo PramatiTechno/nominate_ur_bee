0bef03cf229bb3d77ddd101d4d788b2c939df668
manojpr <manoj.prasanth@imaginea.com>
2019-02-20 19:32:41 +0530

    award index templates

---
+++ nominate_app/award_index.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from nominate_app.models import Awards, AwardTemplate
+from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse 
+import IPython
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def view_awards(request):
+    if request.method == 'GET':
+        forms = Awards.objects.all()
+    return render(request, 'view_awards.html',{'forms':forms})
+
+
+def edit_awards(request,id):
+    if request.method == 'GET':
+        forms = Awards.objects.all()
+    return render(request, 'view_awards.html',{'forms':forms})
+
+
+def award_template_index(request):
+	if request.method == 'GET':
+		forms = Awards.objects.all()
+		id = request.GET.get('id')
+		load_templates = AwardTemplate.objects.all()
+		print (load_templates)
+	return render(request, 'award_template_index.html',{'forms': forms,
+        'load_templates': load_templates,
+    })
+
+def award_template_load(request,id):
+	load_templates = AwardTemplate.objects.filter(award_id=id)
+	print (load_templates)
+	to_json = serializers.serialize('json', load_templates)
+	print (to_json)
+	return HttpResponse(to_json, content_type='application/json')
---
+++ nominate_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from nominate_app import award_index
+
+app_name = 'nominate_app'
+urlpatterns = [  
+	path('', award_index.home, name='home'),
+	path('view_awards/', award_index.view_awards, name='view_awards'),
+	path('edit_awards/<id>', award_index.edit_awards, name='edit_awards'),
+	path('award_template_index/', award_index.award_template_index, name='award_template_index'),
+	path('award_template_load/<id>/', award_index.award_template_load, name='award_template_load'),
+]
