67612f410bddfad17d04b29acfad9c9a15876409
manojpr <manoj.prasanth@imaginea.com>
2019-03-06 17:09:16 +0530

    Updated views

--- nominate_app/views/nomination_status_view.py
+++ nominate_app/views/nomination_status_view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from nominate_app.models import Awards, AwardTemplate, NominationInstance
+from nominate_app.models import Awards, AwardTemplate, NominationInstance, User
 from django.http import HttpResponse
 from django.core import serializers 
 
@@ -15,12 +15,13 @@ def nomination_status(request):
         award_id = award_forms.first().id
         award_template_id = AwardTemplate.objects.get(award_id=award_id, is_active=True).id
         nomination_status = NominationInstance.objects.filter(award_template_id=award_template_id)
-        # import IPython;IPython.embed()
     return render(request, 'nominate_app/nomination_status.html',{'nomination_status':nomination_status, 'award_forms':award_forms})
 
 def nomination_status_load(request,id):
 	award_forms = Awards.objects.all()
 	award_template_id = AwardTemplate.objects.get(award_id=id, is_active=True).id
 	load_templates = NominationInstance.objects.filter(award_template_id=award_template_id)
+	user_name = User.objects.filter(id__in=load_templates.values('user_id'))
+	import IPython;IPython.embed()
 	to_json = serializers.serialize('json', load_templates)
 	return HttpResponse(to_json, content_type='application/json')
