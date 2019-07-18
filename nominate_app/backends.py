839add47dc8579b6fda7db2ca3cc44b70fbd5e0a
muthukrishnank <muthukrishnan.kasiraman@imaginea.com>
2019-07-18 16:09:35 +0530

    created auth groups

--- nominate_app/backends.py
+++ nominate_app/backends.py
@@ -5,13 +5,17 @@ from __future__ import absolute_import, unicode_literals
 from django_cas_ng.backends import CASBackend
 from django.contrib import messages
 from django.conf import settings
-
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.core.exceptions import ImproperlyConfigured
+import django_cas_ng.views
+from django.contrib.auth.models import Group
+from django_cas_ng.middleware import HttpResponseRedirect
 from django_cas_ng.signals import cas_user_authenticated
-from nominate_app.models import UserProfile
-
+from django.contrib.auth.models import AnonymousUser
+from nominate_app.models import UserProfile, UserRole, Role
+from IPython import embed
 
 from .utils import get_cas_client
 
@@ -20,6 +24,11 @@ __all__ = ['CASBackend']
 
 class NominateCASBackend(ModelBackend):
     """CAS authentication backend"""
+
+    def __init__(self):
+        self.title_role_map = {
+            "test": "Manager"
+        }
 
     def authenticate(self, request, ticket, service):
         """Verifies CAS ticket and gets or creates User object"""
@@ -46,105 +55,111 @@ class NominateCASBackend(ModelBackend):
                     attributes.pop(cas_attr_name)
 
         UserModel = get_user_model()
-
         # Note that this could be accomplished in one try-except clause, but
         # instead we use get_or_create when creating unknown users since it has
         # built-in safeguards for multiple threads.
-        if settings.CAS_CREATE_USER:
-            user_kwargs = {
-                UserModel.USERNAME_FIELD: username
-            }
-            if settings.CAS_CREATE_USER_WITH_ID:
-                user_kwargs['id'] = self.get_user_id(attributes)
+        if attributes['jobtitle'] not in self.title_role_map:
 
-            user, created = UserModel._default_manager.get_or_create(**user_kwargs)
-            if created:
-                user = self.configure_user(user)
-        else:
-            created = False
-            try:
-                if settings.CAS_LOCAL_NAME_FIELD:
-                    user_kwargs = {
-                        settings.CAS_LOCAL_NAME_FIELD: username
-
-                    }
-                    user = UserModel._default_manager.get(**user_kwargs)
-                else:
-                    user = UserModel._default_manager.get_by_natural_key(username)
-            except UserModel.DoesNotExist:
-                pass
-
-        if not self.user_can_authenticate(user):
+            HttpResponseRedirect(reverse('cas_ng_login')) # or http response
             return None
-
-        if pgtiou and settings.CAS_PROXY_CALLBACK and request:
-            request.session['pgtiou'] = pgtiou
-
-        if settings.CAS_APPLY_ATTRIBUTES_TO_USER and attributes:
-            # If we are receiving None for any values which cannot be NULL
-            # in the User model, set them to an empty string instead.
-            # Possibly it would be desirable to let these throw an error
-            # and push the responsibility to the CAS provider or remove
-            # them from the dictionary entirely instead. Handling these
-            # is a little ambiguous.
-            user_model_fields = UserModel._meta.fields
-            for field in user_model_fields:
-                # Handle null -> '' conversions mentioned above
-                if not field.null:
-                    try:
-                        if attributes[field.name] is None:
-                            attributes[field.name] = ''
-                    except KeyError:
-                        continue
-                # Coerce boolean strings into true booleans
-                if field.get_internal_type() == 'BooleanField':
-                    try:
-                        boolean_value = attributes[field.name] == 'True'
-                        attributes[field.name] = boolean_value
-                    except KeyError:
-                        continue
-
-            user.__dict__.update(attributes)
-
-            # If we are keeping a local copy of the user model we
-            # should save these attributes which have a corresponding
-            # instance in the DB.
+        else:
             if settings.CAS_CREATE_USER:
-                user.save()
-                user_profile_model_fields = UserProfile._meta.fields
-                userprofile = {}
-                for field in user_profile_model_fields:
-                # Handle null -> '' conversions mentioned above
+                user_kwargs = {
+                    UserModel.USERNAME_FIELD: username
+                }
+                if settings.CAS_CREATE_USER_WITH_ID:
+                    user_kwargs['id'] = self.get_user_id(attributes)
+
+                user, created = UserModel._default_manager.get_or_create(**user_kwargs)
+                if created:
+                    user = self.configure_user(user)
+            else:
+                created = False
+                try:
+                    if settings.CAS_LOCAL_NAME_FIELD:
+                        user_kwargs = {
+                            settings.CAS_LOCAL_NAME_FIELD: username
+
+                        }
+                        user = UserModel._default_manager.get(**user_kwargs)
+                    else:
+                        user = UserModel._default_manager.get_by_natural_key(username)
+                except UserModel.DoesNotExist:
+                    pass
+
+            if not self.user_can_authenticate(user):
+                return None
+
+            if pgtiou and settings.CAS_PROXY_CALLBACK and request:
+                request.session['pgtiou'] = pgtiou
+
+            if settings.CAS_APPLY_ATTRIBUTES_TO_USER and attributes:
+                # If we are receiving None for any values which cannot be NULL
+                # in the User model, set them to an empty string instead.
+                # Possibly it would be desirable to let these throw an error
+                # and push the responsibility to the CAS provider or remove
+                # them from the dictionary entirely instead. Handling these
+                # is a little ambiguous.
+                user_model_fields = UserModel._meta.fields
+                for field in user_model_fields:
+                    # Handle null -> '' conversions mentioned above
                     if not field.null:
                         try:
                             if attributes[field.name] is None:
                                 attributes[field.name] = ''
                         except KeyError:
                             continue
-                # Coerce boolean strings into true booleans
+                    # Coerce boolean strings into true booleans
                     if field.get_internal_type() == 'BooleanField':
                         try:
                             boolean_value = attributes[field.name] == 'True'
                             attributes[field.name] = boolean_value
                         except KeyError:
                             continue
-                    userprofile[field.name] = attributes[field.name]
-                    userprofile["user"] = user
-                UserProfile.objects.get_or_create(**userprofile)
-                
-        # send the `cas_user_authenticated` signal
-        cas_user_authenticated.send(
-            sender=self,
-            user=user,
-            created=created,
-            username=username,
-            attributes=attributes,
-            pgtiou=pgtiou,
-            ticket=ticket,
-            service=service,
-            request=request
-        )
-        return user
+
+                user.__dict__.update(attributes)
+
+                # If we are keeping a local copy of the user model we
+                # should save these attributes which have a corresponding
+                # instance in the DB.
+                if settings.CAS_CREATE_USER:
+                    user.save()
+                    user_profile_model_fields = UserProfile._meta.fields
+                    userprofile = {}
+                    for field in user_profile_model_fields:
+                    # Handle null -> '' conversions mentioned above
+                        if not field.null:
+                            try:
+                                if attributes[field.name] is None:
+                                    attributes[field.name] = ''
+                            except KeyError:
+                                continue
+                    # Coerce boolean strings into true booleans
+                        if field.get_internal_type() == 'BooleanField':
+                            try:
+                                boolean_value = attributes[field.name] == 'True'
+                                attributes[field.name] = boolean_value
+                            except KeyError:
+                                continue
+                        userprofile[field.name] = attributes[field.name]
+                        userprofile["user"] = user
+                    obj, created = UserProfile.objects.get_or_create(**userprofile)
+                    # send the obj to create user role relationship.
+                    self.create_user_role(obj)
+
+            # send the `cas_user_authenticated` signal
+            cas_user_authenticated.send(
+                sender=self,
+                user=user,
+                created=created,
+                username=username,
+                attributes=attributes,
+                pgtiou=pgtiou,
+                ticket=ticket,
+                service=service,
+                request=request
+            )
+            return user
 
     # ModelBackend has a `user_can_authenticate` method starting from Django
     # 1.10, that only allows active user to log in. For consistency,
@@ -152,6 +167,15 @@ class NominateCASBackend(ModelBackend):
     if not hasattr(ModelBackend, 'user_can_authenticate'):
         def user_can_authenticate(self, user):
             return True
+
+
+    def create_user_role(self, user):
+        group, created = Group.objects.get_or_create(name = self.title_role_map[user.jobtitle])
+        UserModel = get_user_model()
+        a_user = UserModel.objects.filter(id=user.user_id)[0]
+        a_user.groups.add(group)
+
+        
 
     def get_user_id(self, attributes):
         """
