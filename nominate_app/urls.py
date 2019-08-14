68d8504b96131bfb8d30b5b8772ee9856cb0edcb
arunb <arun.balan@imaginea.com>
2019-08-14 16:37:33 +0530

    populate with read only tables

--- nominate_app/urls.py
+++ nominate_app/urls.py
@@ -41,9 +41,9 @@ urlpatterns = [
   path("graphs/<award_id>",graphs.load_graph,name="load_graph"),    
 
   path('nomination_status/', nomination_status_view.nomination_status, name='nomination_status'),
-  path('nomination_status_load/<id>/', nomination_status_view.nomination_status_load, name='nomination_status_load'),
-  path('nomination_status_load/awards/<id>/templates/<template_id>', nomination_status_view.nomination_status_load_template, name='nomination_status_load_template'),
-  path('nomination_status_load/awards/<id>/templates/<template_id>/filter', nomination_status_view.nomination_status_load_filter, name="nomination_filter"),
+  path('nomination_status_load/<award_name>/', nomination_status_view.nomination_status_load, name='nomination_status_load'),
+  path('nomination_status_load/awards/<award_name>/templates/<template_name>', nomination_status_view.nomination_status_load_template, name='nomination_status_load_template'),
+  path('nomination_status_load/awards/<award_name>/templates/<template_name>/filter', nomination_status_view.nomination_status_load_filter, name="nomination_filter"),
   path('nomination_feed/', nomination_index.NominationIndexView.as_view(), name='nomination_feed'),
   
   path('nomination_feed/<int:nomination_submittion_id>/', nomination_index.nomination_instance_post, name='nomination_instance_post'),
--- nominate_app/views/nomination_status_view.py
+++ nominate_app/views/nomination_status_view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from nominate_app.models import Awards, AwardTemplate, NominationInstance, User, Nomination, Group
+from nominate_app.models import Awards, AwardTemplate, NominationInstance, User, Nomination, Group, NominationSubmitted
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.core import serializers 
@@ -13,25 +13,29 @@ from django.contrib import messages
 def home(request):
     return render(request, 'base.html')
 
-def get_nomination_data(award_template, page, start_day=None, end_day=None, request=None):
+def get_nomination_data(award_ft_submissions, award_name, award_template_name, page, start_day=None, end_day=None, request=None):
   page = int(page)
   nomination_data = []
-  nominations = None
+  award = Awards.objects.get(name=award_name)
+  award_template = AwardTemplate.objects.get(award=award, template_name=award_template_name)
+
   if start_day and end_day and start_day < end_day:
-    nominations = Nomination.objects.filter(award_template_id=award_template.id, start_day__gte=start_day, end_day__lte=end_day)
+    nominations = Nomination.objects.filter(award_template=award_template, start_day__gte=start_day, end_day__lte=end_day)
   else:
     if start_day and end_day:
       messages.error(request, "End date must be greater than start date.")
 
     nominations = Nomination.objects.filter(award_template_id=award_template.id).order_by('id')
+
   paginator = Paginator(nominations, 9)
   try:
-    numbers = paginator.page(page)
+    nominations = paginator.page(page)
   except PageNotAnInteger:
-    numbers = paginator.page(1)
+    nominations = paginator.page(1)
   except EmptyPage:
-    numbers = paginator.page(paginator.num_pages)
-  for nomination in numbers:
+    nominations = paginator.page(paginator.num_pages)
+
+  for nomination in nominations:
     nd = {
       'start_day': nomination.start_day.strftime('%d %B'),
       'end_day': nomination.end_day.strftime('%d %B'),
@@ -41,62 +45,69 @@ def get_nomination_data(award_template, page, start_day=None, end_day=None, requ
       'nomination_id': nomination.id,
       'instances': []
     }
-    for nomination_instance in nomination.nominationinstance_set.all().order_by('id'):
-      st = None
-      for status in nomination_instance.statuses:
-        if nomination_instance.status == status[1]:
-          st = status[0]
-      
+    for submission in nomination.submissions.all().order_by('id'):      
       nd['instances'].append({
-        'username': nomination_instance.user.username,
-        'status': st,
-        'id': nomination_instance.id,
+        'username': submission.email,
+        'status': submission.get_status(submission.status),
+        'id': submission.id,
       })
     
     nomination_data.append(nd)
-  numbers.object_list = nomination_data
-  return numbers
+
+  nominations.object_list = nomination_data
+  return nominations
+
 
 def nomination_status(request):
   if request.method == 'GET':
-    award = Awards.objects.first()
+    awards =  NominationSubmitted.objects.all().values_list('award_name', flat=True).distinct()
+    award = awards[0]
     page = request.GET.get('page', 1)
-  return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, id=award.id))
+
+  return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, award_name=award))
 
 
-def nomination_status_load(request,id):
+def nomination_status_load(request, award_name):
   if request.method == 'GET':
-    award = Awards.objects.get(id=id)
+    # award = Awards.objects.get(id=id)
     page = request.GET.get('page', 1)
-    return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, id=award.id))
+    return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, award_name=award_name))
 
-def nomination_status_load_filter(request,id, template_id):
+def nomination_status_load_filter(request, award_name, template_name):
   page = request.GET.get('page', 1)
   days = request.GET.getlist('filter')
   start_day = datetime.strptime(days[0], '%m/%d/%Y').date()
   end_day = datetime.strptime(days[1], '%m/%d/%Y').date()
-  return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, id=id,template_id=template_id, start_day=start_day, end_day=end_day, request=request))
+  return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, award_name=award_name, template_name=template_name, start_day=start_day, end_day=end_day, request=request))
 
 
-def get_nomination_details(page, id=None, template_id=None, start_day=None, end_day=None, request=None):
-  awards = Awards.objects.all().order_by('id')
-  if id:
-    award = Awards.objects.get(id=id)
+def get_nomination_details(page, award_name=None, template_name=None, start_day=None, end_day=None, request=None):
+  submissions = NominationSubmitted.objects.all()
+  awards = submissions.values_list('award_name', flat=True).distinct()
+  if award_name:
+    award = award_name
   else:
     award = awards[0]
-  award_templates =  AwardTemplate.objects.filter(award_id=award.id).order_by('id')
+
+  award_ft_submissions = submissions.filter(award_name=award)
+  award_templates = award_ft_submissions.values_list('template_name', flat=True).distinct()
   nomination_data = None
+
   if award_templates:
-    if template_id:
-      award_template = AwardTemplate.objects.get(id=template_id)
+    if template_name:
+      award_template = template_name
     else:
-      award_template = AwardTemplate.objects.first()
-    nomination_data = get_nomination_data(award_template, page, start_day=start_day, end_day=end_day, request=request)
+      award_template = award_templates[0]
+    nomination_data = get_nomination_data(award_ft_submissions, award, award_template, page, start_day=start_day, end_day=end_day, request=request)
   else:
     award_template = None
-  return {'award_categories':awards, 'nominations': nomination_data, 'award_selected': award, 'award_templates': award_templates, 'selected_template': award_template}
 
-def nomination_status_load_template(request, id, template_id):
+  return {'award_categories': awards, 'nominations': nomination_data, 'award_selected': award, 'award_templates': award_templates, 'selected_template': award_template}
+
+
+def nomination_status_load_template(request, award_name, template_name):
   if request.method == 'GET':
     page = request.GET.get('page', 1)
-    return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, id=id, template_id=template_id))
+    return render(request, 'nominate_app/nomination_status.html', get_nomination_details(page, award_name=award_name, template_name=template_name))
+
+
