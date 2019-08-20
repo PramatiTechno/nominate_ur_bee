737c9f1ddcf5fc3f25e057ca528160260bf5c300
Akhil <akhilesh.sharma@imaginea.com>
2019-08-20 19:06:26 +0530

    Removed the dependency from migrations

--- nominate_app/migrations/0001_initial.py
+++ nominate_app/migrations/0001_initial.py
@@ -9,12 +9,9 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     initial = True
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0010_group_group'),
     ]
 
     operations = [
