29ad9a7810eff1f785e0180954f292ac97d5d0d9
muthukrishnank <muthukrishnan.kasiraman@imaginea.com>
2019-08-20 14:45:54 +0530

    improved UI on director approve page

--- nominate_app/views/approvals.py
+++ nominate_app/views/approvals.py
@@ -9,16 +9,21 @@ from django.template.defaulttags import register
 def get_user(value, arg):
     return User.objects.get(id=arg)
 
+@register.filter
+def get_status(submission, status_code):
+    return submission.get_status(status_code)
+
 @group_required('Directorial Board Member', raise_exception=True)
 def index(request):
     
     selected_status = request.GET.get('status', default='reviewed')
     current_user = User.objects.get(id=request.user.id)
     page = request.GET.get('page', 1)
-    statuses = ['reviewed', 'approved']
+    statuses = ['reviewed', 'history']
     if selected_status == 'reviewed':
         nomination_data = NominationSubmitted.objects.filter(status=1)
-        
+    elif selected_status == 'history':
+        nomination_data = NominationSubmitted.objects.filter(status__in=[2, 3, 4])
     paginator = Paginator(nomination_data, 9)
     try:
         nominations = paginator.page(page)
@@ -26,6 +31,7 @@ def index(request):
         nominations = paginator.page(1)
     except EmptyPage:
         nominations = paginator.page(paginator.num_pages)
+    
     return render(request, 'nominate_app/approvals/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': selected_status })
 
 @group_required('Directorial Board Member', raise_exception=True)
@@ -47,4 +53,9 @@ def approve(request, submission_id):
     nomination_submitted = NominationSubmitted.objects.get(id=submission_id)
     ratings = NominationRating.objects.filter(submission_id=nomination_submitted.id)
     avg_rating = NominationRating.objects.filter(submission_id=submission_id).aggregate(Avg('rating'))['rating__avg']
+    if request.GET.get('page', default=None) == 'show':
+        
+        comment = nomination_submitted.director_comment.first().comment
+        status = nomination_submitted.get_status(nomination_submitted.status)
+        return render(request, 'nominate_app/approvals/show.html', {'selected_nomination': nomination_submitted, 'avg_rating': avg_rating, 'ratings': ratings, 'status': status, 'comment': comment})    
     return render(request, 'nominate_app/approvals/new.html', {'selected_nomination': nomination_submitted, 'avg_rating': avg_rating, 'ratings': ratings})
