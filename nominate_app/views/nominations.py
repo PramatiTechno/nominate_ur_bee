917c36e76745b4057db2315b0475d062460545b0
arunb <arun.balan@imaginea.com>
2019-08-27 16:40:03 +0530

    multi select option for groups

--- nominate_app/views/nominations.py
+++ nominate_app/views/nominations.py
@@ -53,6 +53,7 @@ def index(request):
   except EmptyPage:
     nominations = paginator.page(paginator.num_pages)
   return render(request, 'nominate_app/nominations/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': "new" })
+  
 @group_required('Directorial Board Member', 'Technical Jury Member', 'Manager', raise_exception=True)
 def status_index(request, status_value):
   current_user = User.objects.get(id=request.user.id)
