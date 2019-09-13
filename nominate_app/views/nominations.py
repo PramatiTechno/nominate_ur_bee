54d468b40f2439414c00780d82615295d00e1966
arunb <arun.balan@imaginea.com>
2019-09-13 17:42:24 +0530

    changed date restrictions

--- nominate_app/views/nominations.py
+++ nominate_app/views/nominations.py
@@ -20,81 +20,86 @@ def get_item(dictionary, key):
     return dictionary.get(key)
 
 def change_date(request, nomination_id):
-  nomination = Nomination.objects.get(id=nomination_id)
-  date = request.POST['date']
-  nomination.end_day = datetime.strptime(date, '%m/%d/%Y').date()
-  nomination.save()
-  return JsonResponse({
-    'status':'success',
-    'end_day': nomination.end_day.strftime('%d %B'),
-    'end_day_data': nomination.end_day,
-  })
+    nomination = Nomination.objects.get(id=nomination_id)
+    date = request.POST['date']
+    nomination.end_day = datetime.strptime(date, '%m/%d/%Y').date()
+    nomination.save()
+    return JsonResponse({
+        'status':'success',
+        'end_day': nomination.end_day.strftime('%d %B'),
+        'end_day_data': nomination.end_day,
+    })
 
 @group_required('Directorial Board Member', 'Technical Jury Member', 'Manager', raise_exception=True)
 def index(request):
-  
-  current_user = User.objects.get(id=request.user.id)
-  group = current_user.groups.first()
-  nominations = Nomination.objects.filter(group__in=list(map(lambda x: x['id'],current_user.groups.values()))) #,start_day__gt= datetime.today(),end_day__lt= datetime.today() )#list(map(lambda g: g.id,current_user.groups.all()))) 
-  new_nominations = []
-  
-  for nomination in nominations:
-      nomination_instance = nomination.nominationinstance_set.filter(user=request.user)
-      if nomination_instance.count() == 0:
-        new_nominations.append(nomination)   
-  statuses = ['New', 'Saved']
+    requested_user_group = request.user.groups.first()
+    nominations = Nomination.objects.filter(group=requested_user_group, start_day__lte= datetime.today(), end_day__lte= datetime.today())
+    new_nominations = []
+    for nomination in nominations:
+        nomination_instance = nomination.nominationinstance_set.filter(user=request.user)
+        has_question = nomination.award_template.questions_set.filter(groups=requested_user_group).exists() if nomination.award_template else False
+        if nomination_instance.count() == 0 and has_question:
+            new_nominations.append(nomination)   
 
-  if group.name.lower() == "directorial board member":
-    statuses.append('Submitted')
-  else:
-    statuses.extend(status[0] for status in NominationSubmitted.statuses) 
-  page = request.GET.get('page', 1)
-  paginator = Paginator(new_nominations, 9)
-  try:
-    nominations = paginator.page(page)
-  except PageNotAnInteger:
-    nominations = paginator.page(1)
-  except EmptyPage:
-    nominations = paginator.page(paginator.num_pages)
-  return render(request, 'nominate_app/nominations/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': "new" })
+    statuses = ['New', 'Saved']
+
+    if requested_user_group.name.lower() == "directorial board member":
+        statuses.append('Submitted')
+    else:
+        statuses.extend(status[0] for status in NominationSubmitted.statuses) 
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(new_nominations, 9)
+    try:
+        nominations = paginator.page(page)
+    except PageNotAnInteger:
+        nominations = paginator.page(1)
+    except EmptyPage:
+        nominations = paginator.page(paginator.num_pages)
+
+    return render(request, 'nominate_app/nominations/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': "new" })
   
+
 @group_required('Directorial Board Member', 'Technical Jury Member', 'Manager', raise_exception=True)
 def status_index(request, status_value):
-  current_user = User.objects.get(id=request.user.id)
-  group = current_user.groups.first()
-  statuses = ['New', 'Saved']
-  nomination_data = []
-  if group.name.lower() == "directorial board member":
-    statuses.append('Submitted')
-  else:
-    statuses.extend(status[0] for status in NominationSubmitted.statuses) 
-  if status_value.lower() == "new":
-    
-    return redirect('nominate_app:nominations')
-  elif status_value.lower() == "saved":
-    nominations = Nomination.objects.filter(group__in=list(map(lambda x: x['id'],current_user.groups.values())))
-    for nomination in nominations:
-      nomination_instance = nomination.nominationinstance_set.filter(user=request.user)
-      if nomination_instance and nomination_instance[0].status == 1:
-        nomination.nomination_instance_id = nomination_instance[0].id
-        nomination_data.append(nomination)
-  elif status_value.lower() == "submitted":
-    nomination_data = NominationSubmitted.objects.filter(status=0, email=request.user.email)
-  elif status_value.lower() == "reviewed":
-    nomination_data = NominationSubmitted.objects.filter(status=1, email=request.user.email)
-  elif status_value.lower() == "approved":
-    nomination_data = NominationSubmitted.objects.filter(status=2, email=request.user.email)
-  elif status_value.lower() == "dismissed":
-    nomination_data = NominationSubmitted.objects.filter(status=3, email=request.user.email)
-  else:
-    nomination_data = NominationSubmitted.objects.filter(status=4, email=request.user.email)
-  page = request.GET.get('page', 1)
-  paginator = Paginator(nomination_data, 9)
-  try:
-    nominations = paginator.page(page)
-  except PageNotAnInteger:
-    nominations = paginator.page(1)
-  except EmptyPage:
-    nominations = paginator.page(paginator.num_pages)
+    group = request.user.groups.first()
+    statuses = ['New', 'Saved']
+    nomination_data = []
+    status_code = {
+        'submitted': 0,
+        'reviewed': 1,
+        'approved': 2,
+        'dismissed': 3,
+        'on hold': 4,
+    }
+
+    if group.name.lower() == "directorial board member":
+        statuses.append('Submitted')
+    else:
+        statuses.extend(status[0] for status in NominationSubmitted.statuses) 
+
+    if status_value.lower() == "new":
+        return redirect('nominate_app:nominations')
+
+
+    elif status_value.lower() == "saved":
+        nominations = Nomination.objects.filter(group__in=request.user.groups.all(), start_day__lte= datetime.today(), end_day__lte= datetime.today())
+        for nomination in nominations:
+            nomination_instance = nomination.nominationinstance_set.filter(user=request.user)
+            if nomination_instance and nomination_instance[0].status == 1:
+                nomination.nomination_instance_id = nomination_instance[0].id
+                nomination_data.append(nomination)
+
+    elif status_value.lower() in status_code.keys():
+        nomination_data = NominationSubmitted.objects.filter(status=status_code[status_value.lower()], email=request.user.email)
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(nomination_data, 9)
+    try:
+        nominations = paginator.page(page)
+    except PageNotAnInteger:
+        nominations = paginator.page(1)
+    except EmptyPage:
+        nominations = paginator.page(paginator.num_pages)
   
-  return render(request, 'nominate_app/nominations/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': status_value.lower() })
+    return render(request, 'nominate_app/nominations/index.html', {'data': nominations, 'statuses': statuses, 'selected_status': status_value.lower() })
